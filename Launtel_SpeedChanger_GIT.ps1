c039bba91993364c3775fee41fc0b7017e4fb9ee
ryanbop <ryanbrsjv@gmail.com>
2021-02-05 20:43:45 +1000

    Update Launtel_SpeedChanger_GIT.ps1

--- Launtel_SpeedChanger_GIT.ps1
+++ Launtel_SpeedChanger_GIT.ps1
@@ -12,15 +12,8 @@ $password = "" #something something insert ConvertTo-SecureString
 
 
 $params = @{id="login-form";username=$username;password=$password}
-$log = Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
-$log
-Add-Content -Path "C:\Users\$($env:USERNAME)\Downloads\login_$(Get-Date -Format hhmm_ddMM).log" -value $log
+Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
 Write-Host "Connected"
 $change_params = @{psid="$($psid)";locid="$($location_id)";avcid="$($avc_id)";unpause=0;'btn-confirm-back-new-service'="confirm"}
-$ok = Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
-$ok
-Add-Content -Path "C:\Users\$($env:USERNAME)\Downloads\update_$(Get-Date -Format hhmm_ddMM).log" -Value $ok
+Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
 Write-Host "WebRequest Sent, check "
-
-$ok = ""
-$log = ""
