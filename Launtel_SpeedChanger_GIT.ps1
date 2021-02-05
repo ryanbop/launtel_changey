2107a579ce55aa733d43376541f878ae8e34c6cf
ryanbop <ryanbrsjv@gmail.com>
2021-02-06 07:08:50 +1000

    Update Launtel_SpeedChanger_GIT.ps1

--- Launtel_SpeedChanger_GIT.ps1
+++ Launtel_SpeedChanger_GIT.ps1
@@ -12,7 +12,7 @@ $password = "" #something something insert ConvertTo-SecureString
 
 
 $params = @{id="login-form";username=$username;password=$password}
-Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
+Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -SessionVariable $launtel
 Write-Host "Connected"
 $change_params = @{psid="$($psid)";locid="$($location_id)";avcid="$($avc_id)";unpause=0;'btn-confirm-back-new-service'="confirm"}
 Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
