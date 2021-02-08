329ad379f27e2b79ca32a27b6a63abe750d4441c
ryanbop <ryanbrsjv@gmail.com>
2021-02-08 21:01:45 +1000

    Update Launtel_DateChanger_GIT.ps1
    Updated to the working version of the script.

--- Launtel_DateChanger_GIT.ps1
+++ Launtel_DateChanger_GIT.ps1
@@ -9,38 +9,24 @@ $username = ""
 $password = "" #something something insert ConvertTo-SecureString
 
 #Changes Speed if Friday
-if(((Get-Date).Day) -eq 5) {
-    $psid = ""
+if(((Get-Date).DayOfWeek) -eq 'Friday') {
+    $psid = "" <#Change Me#>
 
     $params = @{id="login-form";username=$username;password=$password}
-    $log = Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
-    $log
-    Add-Content -Path "C:\Users\$($env:USERNAME)\Downloads\login_$(Get-Date -Format hhmm_ddMM).log" -value $log
+    Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
     Write-Host "Connected"
     $change_params = @{psid="$($psid)";locid="$($location_id)";avcid="$($avc_id)";unpause=0;'btn-confirm-back-new-service'="confirm"}
-    $ok = Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
-    $ok
-    Add-Content -Path "C:\Users\$($env:USERNAME)\Downloads\update_$(Get-Date -Format hhmm_ddMM).log" -Value $ok
+    Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
     Write-Host "WebRequest Sent, check "
-
-    $ok = ""
-    $log = ""
 }
 #Changes Speed if Monday
-elseif (((Get-Date).Day) -eq 7) {
-    $psid = ""
+elseif (((Get-Date).DayOfWeek) -eq 'Sunday') {
+    $psid = "" <#Change Me#>
 
     $params = @{id="login-form";username=$username;password=$password}
-    $log = Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
-    $log
-    Add-Content -Path "C:\Users\$($env:USERNAME)\Downloads\login_$(Get-Date -Format hhmm_ddMM).log" -value $log
+    Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
     Write-Host "Connected"
     $change_params = @{psid="$($psid)";locid="$($location_id)";avcid="$($avc_id)";unpause=0;'btn-confirm-back-new-service'="confirm"}
-    $ok = Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
-    $ok
-    Add-Content -Path "C:\Users\$($env:USERNAME)\Downloads\update_$(Get-Date -Format hhmm_ddMM).log" -Value $ok
+    Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
     Write-Host "WebRequest Sent, check "
-
-    $ok = ""
-    $log = ""
 }
