ce41db309f18f9c182799e4b4ded6bfe190a288f
ryanbop <ryanbrsjv@gmail.com>
2021-02-10 20:33:15 +1000

    Update Launtel_SpeedChanger_GIT.ps1
    Compacting the code and fixing some of the Write-Hosts (still no logic there)

--- Launtel_SpeedChanger_GIT.ps1
+++ Launtel_SpeedChanger_GIT.ps1
@@ -1,19 +1,14 @@
 #Script made by RyanBop
-
-#info here https://github.com/ryanbop/launtel_changey/blob/main/README.md
+#Information here https://github.com/ryanbop/launtel_changey/wiki
 $service_id = ""
-$avc_id = ""
-$location_id = ""
+$avc_id = "AVC"
+$location_id = "LOC"
 $psid = ""
-#Launtel Creds
 $username = ""
 $password = "" #something something insert ConvertTo-SecureString
-
-
-
+#Logic
 $params = @{id="login-form";username=$username;password=$password}
-Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -SessionVariable $launtel
-Write-Host "Connected"
-$change_params = @{id="";psid="$($psid)";locid="$($location_id)";avcid="$($avc_id)";unpause=0;'btn-confirm-back-new-service'="confirm"}
+Invoke-WebRequest -Uri "https://residential.launtel.net.au/login" -Method Post -Body $params -WebSession $launtel
+$change_params = @{psid="$($psid)";locid="$($location_id)";avcid="$($avc_id)";unpause=0;'btn-confirm-back-new-service'="confirm"}
 Invoke-WebRequest -Uri "https://residential.launtel.net.au/confirm_service?psid=$($psid)&unpause=0&service_id=$($service_id)&upgrade_options=&avcid=$($avc_id)&locid=$($location_id)" -WebSession $launtel -Method POST -Body $change_params
-Write-Host "WebRequest Sent, check "
+Write-Host "WebRequest Sent, check the Launtel Portal https://residential.launtel.net.au/services"
